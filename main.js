c738519d257b799ca132b8189bff7837d33653f1
Rémy Kies <gouru.rk.gouru.rk@gmail.com>
2022-06-28 23:20:42 +0200

    update cell

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 "use strict";
 
-// document.getElementById("one_step").addEventListener("click", refresh);
-document.getElementById("validate").addEventListener("click", get_size);
+document.getElementById("one_step").addEventListener("click", add_step);
+document.getElementById("validate").addEventListener("click", init);
 
-function get_size() {
-    let rows_button = document.getElementById("rows");
-    let columns_button = document.getElementById("columns");
-    let rows = parseInt(rows_button.value);
-    let columns = parseInt(columns_button.value);
-    rows_button.style.display = "none";
-    columns_button.style.display = "none";
+function init() {
+    let size = get_size();
+    let rows = size[0];
+    let columns = size[1];
+    document.getElementById("rows").style.display = "none";
+    document.getElementById("columns").style.display = "none";
     document.getElementById("validate").style.display = "none";
     document.getElementById("one_step").style.display = "block";
     load_grid(rows, columns)
@@ -48,7 +47,10 @@ function count(list, element) {
     }, 0)
 }
 
-function get_neighbor(x, y) {
+function get_neighbors(x, y) {
+    let size = get_size();
+    let rows = size[0];
+    let columns = size[1];
     let neighbors = [
         [x - 1, y - 1],
         [x, y - 1],
@@ -66,7 +68,52 @@ function get_neighbor(x, y) {
     neighbors.forEach(neighbor => {
         abscissa = neighbor[0];
         ordinate = neighbor[1];
-        cell = document.getElementById(`${abscissa}-${ordinate}`);
-        colors.push(cell.className[1]);
+        if (0 <= abscissa && abscissa < columns) {
+            if (0 <= ordinate && ordinate < rows) {
+                cell = document.getElementById(`${abscissa}-${ordinate}`);
+                colors.push(cell.className.split(" ")[1]);
+            }
+        }
+    });
+    return colors;
+}
+
+function get_size() {
+    let rows_button = document.getElementById("rows");
+    let columns_button = document.getElementById("columns");
+    let rows = parseInt(rows_button.value);
+    let columns = parseInt(columns_button.value);
+    return [rows, columns]
+}
+
+function add_step() {
+    let size = get_size();
+    let rows = size[0];
+    let columns = size[1];
+    let cell;
+    let cell_color;
+    let nb_neighbors;
+    let neighbors_colors;
+    let to_change = [];
+    for (let y=0; y < columns; y++) {
+        for (let x=0; x < rows; x++) {
+            neighbors_colors = get_neighbors(x, y);
+            console.log(neighbors_colors)
+            cell = document.getElementById(`${x}-${y}`);
+            cell_color = cell.className.split(" ")[1];
+            nb_neighbors = count(neighbors_colors, "white");
+            if (cell_color === "black" && nb_neighbors === 3) {
+                to_change.push(cell);
+            } else {
+                if (cell_color === "white") {
+                    if (!(nb_neighbors !== 2 && nb_neighbors !== 3)) {
+                        to_change.push(cell)
+                    }
+                }
+            }
+        } 
+    }
+    to_change.forEach(cell => {
+        change_status(cell);
     });
 }
